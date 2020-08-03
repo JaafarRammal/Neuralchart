f1465b83bb13aead07032e2f08694c55538e2307
JaafarRammal <jarammal@gmail.com>
2020-08-03 18:29:17 +0100

    basic setup

---
+++ neuralApp/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  constructor(){
+
+  }
+  
+  ngInit(){
+
+  }
+
+  openConfig(){
+
+  }
+
+  saveConfig(){
+    
+  }
+
+  generatePython(){
+    
+  }
+
+}
