b084844947a5957c78d1f2290881c1949af63617
Adi <agoel4512@gmail.com>
2020-08-04 12:33:47 +0100

    Example json for script generation

--- apiServer/index.py
+++ apiServer/index.py
@@ -1,10 +1,12 @@
 from flask import Flask, send_file
 from .script_generation import generate_script
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 @app.route('/download')
 def download():
