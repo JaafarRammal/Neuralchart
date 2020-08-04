e3685e7a0441970d951f76509052defb213f95e6
Adi <agoel4512@gmail.com>
2020-08-04 13:41:05 +0100

    File download script gen service scaffold done

--- apiServer/src/index.js
+++ apiServer/src/index.js
@@ -1,10 +1,15 @@
+
 const express = require('express')
+const generateScript = require('./scriptGeneration')
 const app = express()
 const port = 3000
 
-app.get('/download', (req, res) => {
+app.get('/', (req, res) => {
+    res.send("Script generation service")
+})
 
-
+app.post('/download', (req, res) => {
+    generateScript(req.body)
     res.download('../scripts/example.py', 'model.py', (err) => {
         if (err) {
             res.send("Error!")
---
+++ apiServer/src/scriptGeneration.js
@@ -0,0 +1,5 @@
+const generateScript = (uploadJSON) => {
+    console.log(uploadJSON)
+};
+
+module.exports = generateScript
